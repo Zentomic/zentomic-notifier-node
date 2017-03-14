d9326eda02fb9f9ae1791640e128e8c09b98dce9
ngoc tuan nguyen <ngoctuan82@gmail.com>
2017-03-14 14:07:36 -0700

    first deploy

--- app/models/user.js
+++ app/models/user.js
@@ -42,5 +42,61 @@ userSchema.methods.validPassword = function(password) {
     return bcrypt.compareSync(password, this.local.password);
 };
 
+
+//-------------------------------------------------------------------------------------------
+// CRUD Section -- mapping the moongse functions
+//Create
+/*
+    var newUser = User({
+      local : {
+                    email        : "abc@gmail.com",
+                    password     : "12345",
+                }
+    });
+    // Create a User model
+    newUser.save(function(err){
+        if (err) throw err;
+        console.log('User created!');
+    });
+*/
+
+
+//Read
+/*
+sample:
+    User.FindOne(
+        { 'local.email' :  "abc@gmail.com" }, 
+        function(err, user) {
+            //custom code here
+            }
+        );
+        
+        
+     User.FindOne(
+        { 'facebook.email' :  "abc@gmail.com" }, 
+        function(err, user) {
+            //custom code here
+            }
+        );
+        
+*/
+// Update
+/* 
+    User.findOneAndUpdate({'local.email':"abc@gmail.com"}, {"local.email":'cba@gmail.com'}, function(err){
+        if(err) throw err;
+    });
+*/
+//Delete
+/*
+    User.findOneAndRemove({'local.email':"abc@gmail.com"}, function(err){
+        if(err) throw err;
+    });
+*/
+
+//---------------------------------- END CRUD SECTION ---------------------------------------
+
+
+//-------------------------------------------------------------------------------------------
+
 // create the model for users and expose it to our app
 module.exports = mongoose.model('User', userSchema);
