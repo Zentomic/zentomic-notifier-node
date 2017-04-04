74edc48a5d89e1ad2e7208ab7e5cb804bb67a0fc
ngoc tuan nguyen <ngoctuan82@gmail.com>
2017-04-04 10:20:30 -0700

    deploy heroku zentomic notifier node better

---
+++ deploy-git.bat
@@ -0,0 +1,9 @@
+git add .
+
+git commit -m "deploy zentomic notifier node Github"
+
+git remote add origin https://github.com/Zentomic/zentomic-notifier-node.git
+
+git remote -v
+
+git push origin master --force
