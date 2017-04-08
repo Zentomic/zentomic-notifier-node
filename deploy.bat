63956b3c59f572425c3ec9b2c6918d868202fb08
ngoc tuan nguyen <ngoctuan82@gmail.com>
2017-04-07 20:18:29 -0700

    Update the server.js for removing the deprecated functions in swagger and bodyParser

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,16 @@
-deploy-git.bat %1
-deploy-heroku.bat %1
+git add .
+
+git commit -m %1
+
+git remote add origin https://github.com/Zentomic/zentomic-notifier-node.git
+
+git remote -v
+
+git push origin master --force
+
+
+heroku login
+
+heroku git:remote -a zentomic-notifier-node
+
+git push heroku master
